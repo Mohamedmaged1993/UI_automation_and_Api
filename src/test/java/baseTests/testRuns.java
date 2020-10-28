0d7b5d43f7c0b0c0de592190e511452a6cd57d88
Mohamed Maged <muhammed.maged1993@outlook.com>
2020-10-29 01:53:07 +0200

    add Search for selenium webdriver step

--- src/test/java/baseTests/testRuns.java
+++ src/test/java/baseTests/testRuns.java
@@ -5,12 +5,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import pageObjects.googleHomePage;
 import utilities.propertiesConfig;
 
 public class testRuns {
 
     WebDriver driver;
     WebDriverWait wait;
+    googleHomePage googlehomepage;
+
     int timeOut = 10;
     propertiesConfig propertiesconfig;
 
@@ -20,8 +24,16 @@ public class testRuns {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         wait = new WebDriverWait(driver, timeOut);
+        googlehomepage = new googleHomePage(driver);
         propertiesconfig = new propertiesConfig();
         driver.get(propertiesconfig.getProperty("googleUrl"));
     }
-    
+
+    @Test(priority = 1)
+    public void successfulTest() {
+
+        googlehomepage.fillSearchData(propertiesconfig.getProperty("validSearchInput"));
+        googlehomepage.selectSearchField();
+
+    }
 }
